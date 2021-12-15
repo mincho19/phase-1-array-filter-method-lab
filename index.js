af49cc97cd71ad7423460e4ca850a6a84934fc06
Min Cho <minsangcho19@gmail.com>
2021-12-15 08:46:27 -0800

    commit tree 64ae0673d764b650fb193f440982984bb87b5d41

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+// function findMatching(collection, cb){
+//     for(const drivers of collection){
+//         if(cb(drivers)){
+//             console.log(user.firstName);
+//         }
+//     }
+// }
+
+
+function findMatching(drivers, bobby){
+    return drivers.filter(function (driverName) {return driverName.toUpperCase() === bobby.toUpperCase()});
+   
+}
+
+function fuzzyMatch(drivers, startingLetter){
+    return drivers.filter(function (driverName) {return driverName.charAt(0) === startingLetter.charAt(0)})
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function (driverObject) {return driverObject['name'] === name});
+}
